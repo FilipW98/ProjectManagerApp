4c2d770f4ca66506294bb500b70d96b326e5e1b1
Filip Woźniak <filip.wozniak98@wp.pl>
2025-01-30 18:38:42 +0100

    added deleteTaskHandler function

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -1,13 +1,37 @@
 import { ProjectDetailsProps } from '../types';
+import {useState } from 'react';
 
 
 export default function ProjectDetails({ selectedProject, projectsData, setSaveProject, setSelectedProject }: ProjectDetailsProps) {
+
+	const [taskData, setTaskData] = useState<{tasks:string[], input:string}>({ tasks:[], input:''});
 
 	function deleteProjectHandler() {
 		const updatedProjects = projectsData.filter(project => project.title !== selectedProject!.title);
         setSaveProject(updatedProjects);
         setSelectedProject(null);
 	}
+
+	function deleteTaskHandler(selectedTask:string) {
+		setTaskData(prev => ({
+			...prev,
+			tasks: prev.tasks.filter(task => task !== selectedTask),
+		}));
+	}
+
+	
+	function addTaskHandler() {
+		console.log(taskData.input);
+
+		if (taskData.input.trim() !== '' && !taskData.tasks.includes(taskData.input.trim())) {
+			setTaskData(prev => ({
+				tasks: [...prev.tasks, prev.input],
+				input: '',
+			}));
+		}
+	}
+
+
 
 	return (
 		<div className='flex-col'>
@@ -26,6 +50,25 @@ export default function ProjectDetails({ selectedProject, projectsData, setSaveP
 				</button>
 			</div>
 			<div className='underline'></div>
+
+			<h3>Tasks</h3>
+			<div>
+				<input
+					value={taskData.input}
+					type='text'
+					onChange={e => setTaskData(prev => ({ ...prev, input: e.target.value }))}
+				/>
+				<button onClick={addTaskHandler}>Add task</button>
+			</div>
+			<ul>
+				{taskData.tasks.map(task => {
+					return <li key={task}>
+						<button onClick={() => { deleteTaskHandler(task); }}>
+						{task}
+						</button>
+					</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
